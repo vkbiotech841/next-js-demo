37fddee8e22153205d6de8c5e3fa9b94f68448e3
VIKRAM KUMAR <vkbiotech841@gmail.com>
2021-02-22 20:23:24 +0530

    article list added

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head';
+import ArticleList from '../components/ArticleList';
 
-export default function Home() {
+export default function Home({ articles }) {
+  console.log("articles", articles);
   return (
 
     <div>
@@ -8,8 +10,22 @@ export default function Home() {
         <title>Music notations</title>
         <meta name='keywords' content='vikram, flute, notation, flute notation, online, classes' />
       </Head>
-      <h1>Welcome to Next</h1>
+
+      <ArticleList articles={articles} />
+
     </div>
 
   )
+}
+
+
+export const getStaticProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+  const articles = await res.json()
+
+  return {
+    props: {
+      articles,
+    },
+  }
 }
