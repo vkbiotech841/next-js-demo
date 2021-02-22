b4d8fc1c0ea85cb5ac1925e7de63e4b171b6d879
VIKRAM KUMAR <vkbiotech841@gmail.com>
2021-02-22 16:40:45 +0530

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+
+const about = () => {
+    return (
+        <div>
+            <Head>
+                <title>Music notations</title>
+                <meta name='keywords' content='vikram kumar, flute artist, notation, flute notation, online, classes' />
+            </Head>
+
+            <h1>About page</h1>
+
+        </div>
+    )
+}
+
+export default about
